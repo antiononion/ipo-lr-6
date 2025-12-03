7127e0bc96c13b05058b9981d460f2a34cfa4217
Anton <antiononion@gmail.com>
2025-12-03 09:11:24 +0300

    main.py

--- task_6/main.py
+++ task_6/main.py
@@ -1,11 +1,15 @@
 import random
 сounto = 0
+#СОЗДАЕМ СПИСОК ИЗ РАНДОМНЫХ ЧИСЕЛ
 lisst = [[random.randint(-10, 10) for i in range(4)] for i in range(20)]
 print("Исходный список из ", len(lisst), " элементов")
+#ВЫВОДИМ СПИСОК
 for tough in lisst:
     print(tough)
+#СОЗДАЕМ КОРТЕЖ ГДЕ ТОЛЬКО УНИКАЛЬНЫЕ ЗНАЧЕНИЯ
 tupler = set()
 for tough in lisst:
+#СОРТИРУЕМ И ДОБАВЛЯЕМ КОМБИНАЦИИ УНИКАЛЬНЫХ ЗНАЧЕНИЙ
     sorte = tuple(sorted(tough))
     tupler.add(sorte)
 ucl = list(tupler)
